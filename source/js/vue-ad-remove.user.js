89fc02bf4c2bfa753647e33d74399d75c5c58e03
lio990527 <lio990527@163.com>
2020-07-31 18:51:48 +0800

    remove more ads

--- source/js/vue-ad-remove.user.js
+++ source/js/vue-ad-remove.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         vue ad remove
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @description  try to take over the world!
 // @author       huozi
 // @include      https://cn.vuejs.org/*
@@ -12,6 +12,13 @@
 (function() {
     'use strict';
     var gg = document.getElementById('sidebar-sponsors-special');
+    var ad = document.getElementById('ad');
+    var ads = document.getElementsByClassName('ad-pagetop');
     gg.parentNode.removeChild(gg);
+    ad.parentNode.removeChild(ad);
+    for (var i = 0; i < ads.length; i++) {
+    	ad = ads[i];
+    	ad.parentNode.removeChild(ad);
+    }
     // Your code here...
 })();
